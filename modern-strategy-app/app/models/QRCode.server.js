0eff3c75416902546ec9ad326f527969bf815fc3
Bui The Hoa <buithehoa@gmail.com>
2024-11-21 15:26:46 +0700

    Add QR code query methods.

---
+++ modern-strategy-app/app/models/QRCode.server.js
@@ -0,0 +1,22 @@
+export async function getQRCode(id, graphql) {
+  const qrCode = await db.qrCode.findFirst({ where: { id } });
+
+  if (!qrCode) {
+    return null;
+  }
+
+  return supplementQRCode(qrCode, graphql);
+}
+
+export async function getQRCodes(shop, graphql) {
+  const qrCodes = await db.qrCode.findMany({
+    where: { shop },
+    orderBy: { id: 'desc' }
+  });
+
+  if (qrCodes.length === 0) return [];
+
+  return Promise.all(
+    qrCodes.map((qrCode) => supplementQRCode(qrCode, graphql))
+  );
+}
