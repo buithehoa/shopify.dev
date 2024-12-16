94a4c7192b8e8db6cfaf760a6b08a9854e82dc42
Bui The Hoa <buithehoa@gmail.com>
2024-12-16 15:58:23 +0700

    Get QR code image

--- modern-strategy-app/app/models/QRCode.server.js
+++ modern-strategy-app/app/models/QRCode.server.js
@@ -1,5 +1,9 @@
+import qrcode from "qrcode";
+import invariant from "tiny-invariant";
+import db from "../db.server";
+
 export async function getQRCode(id, graphql) {
-  const qrCode = await db.qrCode.findFirst({ where: { id } });
+  const qrCode = await db.qRCode.findFirst({ where: { id } });
 
   if (!qrCode) {
     return null;
@@ -9,9 +13,9 @@ export async function getQRCode(id, graphql) {
 }
 
 export async function getQRCodes(shop, graphql) {
-  const qrCodes = await db.qrCode.findMany({
+  const qrCodes = await db.qRCode.findMany({
     where: { shop },
-    orderBy: { id: 'desc' }
+    orderBy: { id: "desc" },
   });
 
   if (qrCodes.length === 0) return [];
@@ -19,4 +23,79 @@ export async function getQRCodes(shop, graphql) {
   return Promise.all(
     qrCodes.map((qrCode) => supplementQRCode(qrCode, graphql))
   );
+}
+
+export function getQRCodeImage(id) {
+  const url = new URL(`/qrcodes/${id}/scan`, process.env.SHOPIFY_APP_URL);
+  return qrcode.toDataURL(url.href);
+}
+
+export function getDestinationUrl(qrCode) {
+  if (qrCode.destination === "product") {
+    return `https://${qrCode.shop}/products/${qrCode.productHandle}`;
+  }
+
+  const match = /gid:\/\/shopify\/ProductVariant\/([0-9]+)/.exec(qrCode.productVariantId);
+  invariant(match, "Unrecognized product variant ID");
+
+  return `https://${qrCode.shop}/cart/${match[1]}:1`;
+}
+
+async function supplementQRCode(qrCode, graphql) {
+  const qrCodeImagePromise = getQRCodeImage(qrCode.id);
+
+  const response = await graphql(
+    `
+      query supplementQRCode($id: ID!) {
+        product(id: $id) {
+          title
+          images(first: 1) {
+            nodes {
+              altText
+              url
+            }
+          }
+        }
+      }
+    `,
+    {
+      variables: {
+        id: qrCode.productId,
+      },
+    }
+  );
+
+  const {
+    data: { product },
+  } = await response.json();
+
+  return {
+    ...qrCode,
+    productDeleted: !product?.title,
+    productTitle: product?.title,
+    productImage: product?.images?.nodes[0]?.url,
+    productAlt: product?.images?.nodes[0]?.altText,
+    destinationUrl: getDestinationUrl(qrCode),
+    image: await qrCodeImagePromise,
+  };
+}
+
+export function validateQRCode(data) {
+  const errors = {};
+
+  if (!data.title) {
+    errors.title = "Title is required";
+  }
+
+  if (!data.productId) {
+    errors.productId = "Product is required";
+  }
+
+  if (!data.destination) {
+    errors.destination = "Destination is required";
+  }
+
+  if (Object.keys(errors).length) {
+    return errors;
+  }
 }
